57754e8e33137b9489a2f31dc982a9d53a6564f4
Allen Liu <allen6qi@gmail.com>
2020-07-15 17:53:42 +0800

    changed config file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import "./App.css";
-import axios from "axios";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Searchform from "./components/searchform";
-import Navigation from "./components/nav";
-import Gallery from "./components/gallery";
-import Apikey from "./config";
-import PageNotfound from "./components/pagenotfound";
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Searchform from './components/searchform';
+import Navigation from './components/nav';
+import Gallery from './components/gallery';
+import PageNotfound from './components/pagenotfound';
+import Apikey from './config';
 
 class App extends Component {
   constructor(props) {
@@ -17,65 +17,65 @@ class App extends Component {
       dogs: [],
       horses: [],
       searchItem: [],
-      loading: true
+      loading: true,
     };
   }
 
   // fetch data for three default link.
-  defaultSearch = tag => {
+  defaultSearch = (tag) => {
     axios
       .get(
         `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${Apikey}&tags=${tag}&per_page=24&format=json&nojsoncallback=1`
       )
-      .then(response => {
+      .then((response) => {
         this.setState({
-          [tag]: response.data.photos.photo
+          [tag]: response.data.photos.photo,
         });
       })
-      .catch(error => {
-        console.log("Error fetching or pharsing data", error);
+      .catch((error) => {
+        console.log('Error fetching or pharsing data', error);
       });
   };
 
   //fetch data for searchitem
-  search = (tag = "sunrise") => {
+  search = (tag = 'sunrise') => {
     axios
       .get(
         `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${Apikey}&tags=${tag}&per_page=24&format=json&nojsoncallback=1`
       )
-      .then(response => {
+      .then((response) => {
         this.setState({
           searchItem: response.data.photos.photo,
-          loading: false
+          loading: false,
         });
       })
-      .catch(error => {
-        console.log("Error fetching or pharsing data", error);
+      .catch((error) => {
+        console.log('Error fetching or pharsing data', error);
       });
   };
 
   //reset loading to true when form submited
   toggleLoading = () => {
     this.setState({
-      loading: true
+      loading: true,
     });
   };
 
   //fetch data for four default page when app loads
   componentDidMount() {
     this.search();
-    this.defaultSearch("cats");
-    this.defaultSearch("dogs");
-    this.defaultSearch("horses");
+    this.defaultSearch('cats');
+    this.defaultSearch('dogs');
+    this.defaultSearch('horses');
   }
 
   render() {
     return (
       <Router>
-        <div className="container">
+        <div className='container'>
           <Route
-            path="/"
-            render={props => (
+            path='/'
+            render={(props) => (
               <Searchform
                 {...props}
                 onSearch={this.search}
@@ -87,7 +87,7 @@ class App extends Component {
           <Switch>
             <Route
               exact
-              path="/"
+              path='/'
               render={() =>
                 this.state.loading ? (
                   <p>loading</p>
@@ -97,8 +97,8 @@ class App extends Component {
               }
             />
             <Route
-              path="/search/:tag"
-              render={props =>
+              path='/search/:tag'
+              render={(props) =>
                 this.state.loading ? (
                   <p>loading</p>
                 ) : (
@@ -107,19 +107,19 @@ class App extends Component {
               }
             />
             <Route
-              path="/cat"
-              render={props => <Gallery data={this.state.cats} />}
+              path='/cat'
+              render={(props) => <Gallery data={this.state.cats} />}
             />
             <Route
-              path="/dog"
-              render={props => <Gallery data={this.state.dogs} />}
+              path='/dog'
+              render={(props) => <Gallery data={this.state.dogs} />}
             />
             <Route
-              path="/horse"
-              render={props => <Gallery data={this.state.horses} />}
+              path='/horse'
+              render={(props) => <Gallery data={this.state.horses} />}
             />
 
-            <Route render={props => <PageNotfound />} />
+            <Route render={(props) => <PageNotfound />} />
           </Switch>
         </div>
       </Router>
